2242946dc7b0225e25b5c623f0a1c1dbc9ef0b37
NitinGupta2321 <nitin.gupta2321@gmail.com>
2020-01-27 10:01:52 +0530

    logo image correction

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import TabView from './container/tabview/TabView';
 import Image from './component/image/Image';
-import logo from './filestore/logo.svg';
+import logo from './filestore/logo192.png';
 
 const AppWrapper = styled.div`
   max-width: calc(768px + 60px * 2);
@@ -27,7 +27,7 @@ const App = (props) => {
     return (
         <AppWrapper>
             <HeaderDiv>
-                <Image src={logo} width={60} height={50} mode='fit' />
+                <Image src={logo} width={60} height={40} mode='fit' />
                 <HeaderText>ToDo App</HeaderText>
             </HeaderDiv>
             <TabView/>
